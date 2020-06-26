eade2b0327d8395894d32459022e67f35bbf1821
DKendle <dina.kendle@gmail.com>
2020-06-26 15:28:28 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions
+        set :sessions_secret, "secret"
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        sessions[:item] = params[:item]
+        @items = sessions[:item]
+   
+        erb :checkout
+    end
 end
